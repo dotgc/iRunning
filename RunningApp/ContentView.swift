2977653f731dbe3e5bdf0846c049a5dd0aedc422
Gaurav Chauhan <gaurav@swiftace.in>
2021-07-02 13:50:33 -0700

    Initial Commit

---
+++ RunningApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RunningApp
+//
+//  Created by Gaurav Chauhan on 7/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RunningApp/RunningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningAppApp.swift
+//  RunningApp
+//
+//  Created by Gaurav Chauhan on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct RunningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
